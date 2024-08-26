cadd212512d3cf8b6cfe04603326240fc1282a73
Alekseypavlov14 <aleshapavlov9@gmail.com>
2024-08-26 18:31:49 +0300

    fixed type bug

--- src/validation/create-form-validator.ts
+++ src/validation/create-form-validator.ts
@@ -7,9 +7,11 @@ export function createFormValidator<T extends AnyForm>(config: Partial<Validatio
     const errors: FormErrors<T> = {} as FormErrors<T>
 
     Object.keys(data).forEach(key => {
-      if (!config[key]) return 
+      const validator = config[key]
 
-      if (!config[key](data[key])) {
+      if (!validator) return 
+
+      if (!validator(data[key])) {
         errors[key as keyof T] = `${key} field is invalid`
       }
     })
